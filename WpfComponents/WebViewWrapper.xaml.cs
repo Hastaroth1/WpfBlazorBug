677c833e12345ddda62404c1ec2bb13b34fae6e5
Émile Dupont-Foisy <emile.dupont-foisy@ubisoft.com>
2024-03-07 10:45:22 -0500

    Initial commit

---
+++ WpfComponents/WebViewWrapper.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfComponents;
+
+/// <summary>
+/// Interaction logic for WebViewWrapper.xaml
+/// </summary>
+public partial class WebViewWrapper
+{
+    public WebViewWrapper()
+    {
+        InitializeComponent();
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddWpfBlazorWebView();
+        Resources.Add("services", serviceCollection.BuildServiceProvider());
+    }
+}
